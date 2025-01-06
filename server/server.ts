b2fb456a799ad10ca1560523c83fd361a459c696
Jowoon Lee <jwnle31@gmail.com>
2025-01-06 01:27:26 -0500

    remove host parameter

--- server/server.ts
+++ server/server.ts
@@ -6,7 +6,7 @@ const server: Server = new Server(app);
 const PORT: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 8080;
 
 app
-  .listen(PORT, "localhost", function () {
+  .listen(PORT, function () {
     console.log(`Server is running on port ${PORT}.`);
   })
   .on("error", (err: any) => {
