e7d97f253b218d5191b3acbc661f45ee81d077d9
Jowoon Lee <jwnle31@gmail.com>
2025-01-06 20:59:06 -0500

    replace test env vars

--- server/src/config/db.config.ts
+++ server/src/config/db.config.ts
@@ -3,9 +3,9 @@ import path from "path";
 dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
 export default {
-  HOST: "mysql-10e17dc4-cubing-stats.g.aivencloud.com",
-  USER: "avnadmin",
-  PORT: 17828,
-  PASSWORD: "AVNS_RP96LnKw5fYtEmvGgjE",
-  DB: "defaultdb",
+  HOST: process.env.DB_HOST,
+  USER: process.env.DB_USERNAME,
+  PORT: Number(process.env.DB_PORT),
+  PASSWORD: process.env.DB_PASSWORD,
+  DB: process.env.DB_NAME,
 };
